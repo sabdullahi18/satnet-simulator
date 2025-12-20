d90ba6675681820d293d6a836c143dad98806e60
Sara Abdullahi <sara.abdullahi.22@ucl.ac.uk>
2025-12-20 15:05:50 +0000

    logic for traversing paths

---
+++ internal/network/path.go
@@ -0,0 +1,35 @@
+package network
+
+import (
+	"fmt"
+	"math/rand"
+	"satnet-simulator/internal/engine"
+)
+
+type SatellitePath struct {
+	Name       string
+	Delay      float64 
+	SpikeProb  float64 
+	SpikeDelay float64 
+}
+
+type Destination interface {
+	Receive(sim *engine.Simulation, pkt Packet, pathUsed string)
+}
+
+func (p SatellitePath) Traverse(sim *engine.Simulation, pkt Packet, dest Destination) {
+	totalDelay := p.Delay
+
+	jitter := 0.5 + rand.Float64()*(2.0-0.5)
+	totalDelay += jitter
+
+	if rand.Float64() < p.SpikeProb {
+		fmt.Printf(" [!] DELAY EVENT: Packet %d delayed by %.2fs on %s\n", 
+			pkt.ID, p.SpikeDelay, p.Name)
+		totalDelay += p.SpikeDelay
+	}
+
+	sim.Schedule(totalDelay, func() {
+		dest.Receive(sim, pkt, p.Name)
+	})
+}
