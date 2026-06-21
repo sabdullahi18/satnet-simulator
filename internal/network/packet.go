2b70f97cba1791eddbc2d1a55ce1d05800b8676f
Sara Abdullahi <sara.abdullahi.22@ucl.ac.uk>
2026-06-21 15:39:36 +0100

    duplicate logic in types.go, all in packet.go

--- internal/network/packet.go
+++ internal/network/packet.go
@@ -1,18 +1,26 @@
 package network
 
 type Packet struct {
-	ID           int
-	BatchID      int
-	Src          string
-	CreationTime float64
-	IsFlagged    bool
+	ID       int
+	BatchID  int
+	Src      string
+	SentTime float64
+
+	BaseDelay         float64
+	IncompetenceDelay float64
+	DeliberateDelay   float64
+	ActualDelay       float64
+
+	WasDelayed      bool
+	HasIncompetence bool
+	IsFlagged       bool
 }
 
 func NewPacket(id, batchID int, src string, time float64) Packet {
 	return Packet{
-		ID:           id,
-		BatchID:      batchID,
-		Src:          src,
-		CreationTime: time,
+		ID:       id,
+		BatchID:  batchID,
+		Src:      src,
+		SentTime: time,
 	}
 }
